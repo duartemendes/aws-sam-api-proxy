3ec92be9211f7bd20a0be45ec1a28cc7d0662f2d
Duarte Mendes <duartecostamendes@gmail.com>
2020-05-01 18:57:46 +0100

    feat: Spin up lambda containers

--- index.js
+++ index.js
@@ -1,33 +1,106 @@
 import http from 'http';
-import { readFileSync } from 'fs';
+import { createWriteStream, readFileSync } from 'fs';
 import yaml from 'js-yaml';
-import envVars from './fixtures/envVars.json';
+import Docker, { Image } from 'dockerode';
+import snakeCase from 'lodash.snakecase';
+import envVars from './fixtures/envVars-temp.json';
+
+const PORT = 3000;
+const DOCKER_IMAGE = 'lambci/lambda:nodejs12.x';
 
 const templateYaml = readFileSync('./fixtures/template.yaml', 'utf-8');
 const templateYamlWithoutFns = templateYaml.replace(/!/g, '');
 const template = yaml.safeLoad(templateYamlWithoutFns);
 
-const functions = Object.entries(envVars).reduce((result, [functionName, environment]) => {
+const isApiEvent = ({ Type }) => Type.includes('Api');
+const docker = new Docker();
+
+const parseFunctions = () => Object.entries(envVars).reduce((result, [functionName, environment], i) => {
   const resource = template.Resources[functionName];
   if (!resource) throw new Error(`Function with name "${functionName}" not found in SAM template`);
 
-  const apiEvent = Object.values(resource.Properties.Events).find(({ Type }) => Type.includes('Api'));
+  const { Events, Handler } = resource.Properties;
+
+  const apiEvent = Object.values(Events).find(isApiEvent);
   if (!apiEvent) throw new Error(`Api event not found for function with name "${functionName}"`);
-  const { Properties: { Path, Method } } = apiEvent;
+
+  const { Type, Properties: { Path, Method } } = apiEvent;
 
   return result.concat({
     name: functionName,
+    handler: Handler,
+    eventType: Type,
     path: Path,
     method: Method,
+    containerPort: PORT + i + 1,
     environment,
   });
 }, []);
 
-const server = http.createServer();
+const createContainers = async (functions) => {
+  const promises = functions.map(async ({ name, environment, containerPort, handler }) => {
+    const options = {
+      Image: DOCKER_IMAGE,
+      name: `${snakeCase(name)}_lambda`,
+      Cmd: [handler],
+      Env: [
+        'DOCKER_LAMBDA_WATCH=1',
+        'DOCKER_LAMBDA_STAY_OPEN=1',
+        ...Object.entries(environment).map(([key, value]) => `${key}=${value}`)
+      ],
+      ExposedPorts: {
+        '9001/tcp': {}
+      },
+      Volumes: {
+        '/var/task': {}
+      },
+      HostConfig: {
+        Binds: ['/Users/duartemendes/deadhappy/repos/deathwish-api/dist:/var/task:ro,delegated'],
+        PortBindings: {
+          '9001/tcp': [{ HostPort: `${containerPort}` }]
+        },
+        NetworkMode: 'deadhappy_network'
+      }
+    };
 
-server.on('request', async (req, res) => {
-  console.log('Received request');
-  res.end(JSON.stringify({ status: 'ok', data: { functions, template } }));
-});
+    const container = await docker.createContainer(options);
+    console.log('Starting container', { id: container.id, name: options.name, exposedPort: containerPort });
+    await container.start()
+    return container.id;
+  });
 
-server.listen(3000);
+  return Promise.all(promises);
+}
+
+const spinUpServer = (functions) => {
+  const server = http.createServer();
+
+  server.on('request', async (req, res) => {
+    const { url, method, headers } = req;
+    console.log('Received request', { url, method, headers });
+
+    // TODO: pipe request to corresponding container... might need to map querystring, headers, path parameters...
+
+    res.end(JSON.stringify({ status: 'ok', data: { functions } }));
+  });
+
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+async function go() {
+  const ping = await docker.ping();
+  if (ping.toString() !== 'OK') throw new Error('Docker needs to be running');
+
+  try {
+    const functions = parseFunctions();
+    const containers = await createContainers(functions);
+
+    spinUpServer(functions);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+go();
