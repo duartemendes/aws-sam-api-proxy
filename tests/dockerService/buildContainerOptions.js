4e10a332901d94611f2139355a5b594e96c0980a
Duarte Mendes <duartecostamendes@gmail.com>
2020-05-03 12:45:42 +0100

    test: cover build container options

---
+++ tests/dockerService/buildContainerOptions.js
@@ -0,0 +1,70 @@
+import buildContainerOptions from '../../src/dockerService/buildContainerOptions';
+
+describe('buildContainerOptions()', () => {
+  const apiName = 'test-api';
+  const distPath = './dist';
+  const dockerNetwork = 'test_network';
+
+  beforeAll(() => {
+    process.env.API_NAME = apiName;
+    process.env.DIST_PATH = distPath;
+    process.env.DOCKER_NETWORK = dockerNetwork;
+  });
+
+  afterAll(() => {
+    delete process.env.API_NAME;
+    delete process.env.DIST_PATH;
+    delete process.env.DOCKER_NETWORK;
+  });
+
+  it('should build container options from function data', () => {
+    const functionData = {
+      name: 'GetResource',
+      environment: {
+        DB_NAME: 'test_database',
+        FEATURE_FLAG: '10%',
+      },
+      containerPort: 3001,
+      handler: 'GetResourceHandler.default',
+      dockerImageWithTag: 'lambci/lambda:nodejs12.x',
+    };
+
+    const containerOptions = buildContainerOptions(functionData);
+
+    expect(containerOptions).toEqual({
+      Image: functionData.dockerImageWithTag,
+      name: 'get_resource_lambda',
+      Cmd: [
+        functionData.handler,
+      ],
+      Env: [
+        'DOCKER_LAMBDA_WATCH=1',
+        'DOCKER_LAMBDA_STAY_OPEN=1',
+        'DB_NAME=test_database',
+        'FEATURE_FLAG=10%',
+      ],
+      Labels: {
+        'aws-sam-api-proxy.api': apiName,
+      },
+      ExposedPorts: {
+        '9001/tcp': {},
+      },
+      Volumes: {
+        '/var/task': {},
+      },
+      HostConfig: {
+        Binds: [
+          `${distPath}:/var/task/ro,delegated`,
+        ],
+        PortBindings: {
+          '9001/tcp': [
+            {
+              HostPort: `${functionData.containerPort}`,
+            },
+          ],
+        },
+        NetworkMode: dockerNetwork,
+      },
+    });
+  });
+});
