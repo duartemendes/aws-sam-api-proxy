5ebf95b5468a27c5384c319970845480e999d32a
Duarte Mendes <duartecostamendes@gmail.com>
2020-09-08 00:03:15 +0100

    test: fix test description

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -68,7 +68,7 @@ describe('index', () => {
     expect(parseFunctionsFromTemplate.mock.calls[0][1]).toEqual({});
   });
 
-  it('should parse parameters correctly', async () => {
+  it('should parse ref-overrides correctly', async () => {
     await startApi(dockerServiceStub, { ...options, refOverrides: 'A=1,B=two,API_KEY=tok_1234' });
 
     expect(parseFunctionsFromTemplate).toHaveBeenCalledTimes(1);
